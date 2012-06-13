6ea8b78e32ea5add92a181c7f8169b5090a2b556
Evgene Oskin <burnst@mail.ru>
2012-06-13 11:34:13 +0700

    3 gtests (for all counter methodes - symbol(), word(), line())

--- test/gtest_wc.cpp
+++ test/gtest_wc.cpp
@@ -1,11 +1,24 @@
 #include "/Users/eugen/WIP_wcpp/src/def_wc.h"
 #include "/Users/eugen/gtest-1.6.0/include/gtest/gtest.h"
 
-TEST(Counter1, test_input)
+TEST(Counter_symbol, test_input)
+{
+  std::string file_name("test//test_input.txt");
+  counter test_count(file_name);
+  EXPECT_EQ (43, test_count.symbol());
+}
+
+TEST(Counter_word, test_input)
+{
+  std::string file_name("test//test_input.txt");
+  counter test_count(file_name);
+  EXPECT_EQ (10,test_count.word());
+}
+
+TEST(Counter_line, test_input)
 {
   std::string file_name("test//test_input.txt");
   counter test_count(file_name);
   EXPECT_EQ (4, test_count.line());
-  EXPECT_EQ (40, test_count.symbol());
-  EXPECT_EQ (10,test_count.word());
 }
+
